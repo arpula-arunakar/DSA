9dfc92d197178f9686e9a8eeada0f4e65443a835
Arunakar Arpula <arunakararpula@outlook.com>
2024-10-11 11:15:14 +0530

    added main code in csharp

--- DSA_CSHARP/Program.cs
+++ DSA_CSHARP/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using DSA_CSHARP.Recursion;
+
 Console.WriteLine("Hello, World!");
+var programs = GetPrograms();
+Console.WriteLine("Select program to run");
+Console.WriteLine("-------------------------------");
+foreach (var program in programs)
+{
+    Console.WriteLine($"{program.Key}. {program.Value}");
+}
+Console.WriteLine("-------------------------------");
+var inputProgram = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(programs.ContainsKey(inputProgram) ? programs[inputProgram] : "Not Found");
+var output = inputProgram switch
+{
+    1 => new SumOfNNumbers().Main(),
+    2 => new Factorial().Main(),
+    3 => new Power().Main(),
+    4 => new TaylorSeries().Main(),
+    5 => new FibanocciSeries().Main(),
+    6 => new nCr().Main(),
+    _ => "Not Found"
+};
+Console.ReadLine();
+
+static Dictionary<int, string> GetPrograms()
+{
+
+    var programs = new Dictionary<int, string>();
+
+    programs.Add(1, "Sum of 'N' Natural Numbers - Recursion");
+    programs.Add(2, "Factorial - Recursion");
+    programs.Add(3, "Power - Recursion");
+    programs.Add(4, "Taylor Series - Recursion");
+    programs.Add(5, "Fibanocci - Recursion");
+    programs.Add(6, "nCr - Recursion");
+    return programs;
+}
